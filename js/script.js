ecd08c6b885f1ea9982d724587ed5aa9e880ae87
alifbinhossain <you@example.com>
2021-09-03 10:05:52 +0600

    new spinner added

--- js/script.js
+++ js/script.js
@@ -10,9 +10,25 @@ const toggleBanner = (displayPro, imgSrc) => {
 };
 const container = document.getElementById("container");
 const diffMessage = document.getElementById("diff-message");
-const spinner = `<div class="spinner-border text-light mt-3" role="status">
-                    <span class="visually-hidden">Loading...</span>
-                 </div>`;
+// const spinner = `<div class="spinner-border text-light mt-3" role="status">
+//                     <span class="visually-hidden">Loading...</span>
+//                  </div>`;
+const spinner = `
+<div class="sk-fading-circle mt-3">
+<div class="sk-circle1 sk-circle"></div>
+<div class="sk-circle2 sk-circle"></div>
+<div class="sk-circle3 sk-circle"></div>
+<div class="sk-circle4 sk-circle"></div>
+<div class="sk-circle5 sk-circle"></div>
+<div class="sk-circle6 sk-circle"></div>
+<div class="sk-circle7 sk-circle"></div>
+<div class="sk-circle8 sk-circle"></div>
+<div class="sk-circle9 sk-circle"></div>
+<div class="sk-circle10 sk-circle"></div>
+<div class="sk-circle11 sk-circle"></div>
+<div class="sk-circle12 sk-circle"></div>
+</div>
+ `;
 let inputValue;
 toggleBanner("block", "yellow-poster");
 
